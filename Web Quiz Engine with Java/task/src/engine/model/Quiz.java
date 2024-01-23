bafd5e31ef1ba5d777c543c638a5253f449c0d9e
Ivan Nekrasov <ivanvinekrasov@google.com>
2024-01-23 14:12:40 +0300

    Created: - Quiz model with fields:      - title      - text      - options - QuizController with methods:      - GET /api/quiz to getting mock Quiz instance with JSON representation in response      - POST /api/quiz with answer parameter, returning AnswerStatus record as JSON representation response

---
+++ Web Quiz Engine with Java/task/src/engine/model/Quiz.java
@@ -0,0 +1,42 @@
+package engine.model;
+
+import java.util.ArrayList;
+
+public class Quiz {
+    String title;
+    String text;
+    ArrayList<String> options;
+
+    public Quiz() {
+    }
+
+    public Quiz(String title, String text, ArrayList<String> options) {
+        this.title = title;
+        this.text = text;
+        this.options = options;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ArrayList<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(ArrayList<String> options) {
+        this.options = options;
+    }
+}
